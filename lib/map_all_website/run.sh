9b05dc54d06208fd2c9dd37226cdf42cae88176d
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-02-06 16:59:52 +0100

    fix execution of map_all_website

--- lib/map_all_website/run.sh
+++ lib/map_all_website/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(ls -v results/*.mapreduced.csv); do
+for i in $(ls -v results/${1}); do
   ruby lib/map_all_website/mapper.rb $i
 done
 
@@ -9,6 +9,6 @@ for i in $(ls -v tmp/*.tagged.csv); do
   tail $i -n +2 >> tmp/first_aggregation.csv
 done
 
-ruby lib/map_all_website/reducer.rb tmp/first_aggregation.csv
+ruby lib/map_all_website/reducer.rb tmp/first_aggregation.csv ${1:2:2}
 
 rm -f tmp/*.csv
