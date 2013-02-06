a33c26c6388f937ddffe05914393c8ba3b8914d4
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-02-06 15:29:57 +0100

    add 15 page dataset

--- executer.sh
+++ executer.sh
@@ -4,6 +4,7 @@ file="scraped/${1// /_}"
 
 file50="${file}-50.csv"
 file20="${file}-20.csv"
+file15="${file}-15.csv"
 file10="${file}-10.csv"
 
 ruby crawler.rb 50 $1
@@ -11,6 +12,9 @@ bash mapreducer.sh $file50
 
 bash get_smaller_dataset.sh $file50 20
 bash mapreducer.sh $file20
+
+bash get_smaller_dataset.sh $file15 15
+bash mapreducer.sh $file15
 
 bash get_smaller_dataset.sh $file20 10
 bash mapreducer.sh $file10
