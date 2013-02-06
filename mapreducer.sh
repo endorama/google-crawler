23ddacf26530fb1b48c7094ec3f4aed8b1d30ab4
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-02-06 15:37:58 +0100

    add map_all_website to mapreduce step

--- mapreducer.sh
+++ mapreducer.sh
@@ -14,4 +14,8 @@ echo "Mapping website frequency to position"
 bash lib/map_frequency_position/run.sh $1
 echo "[DONE]"
 
+echo "Mapping website frequency to position"
+bash lib/map_all_website/run.sh
+echo "[DONE]"
+
 echo "Applying mapreduce... [DONE]"
