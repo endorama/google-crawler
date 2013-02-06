0f9bb10231b251b27b83af98be6cc3014116976d
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-02-06 16:58:20 +0100

    remove map_all_website from mapreduce process

--- mapreducer.sh
+++ mapreducer.sh
@@ -14,8 +14,4 @@ echo "Mapping website frequency to position"
 bash lib/map_frequency_position/run.sh $1
 echo "[DONE]"
 
-echo "Mapping website frequency to position"
-bash lib/map_all_website/run.sh
-echo "[DONE]"
-
 echo "Applying mapreduce... [DONE]"
