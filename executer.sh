0e3155e1bfdb64562793ba1de25a43342aab7ea9
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-02-10 19:47:20 +0100

    fix bug in executer.sh paths

--- executer.sh
+++ executer.sh
@@ -13,8 +13,8 @@ bash mapreducer.sh $file50
 bash get_smaller_dataset.sh $file50 20
 bash mapreducer.sh $file20
 
-bash get_smaller_dataset.sh $file15 15
+bash get_smaller_dataset.sh $file20 15
 bash mapreducer.sh $file15
 
-bash get_smaller_dataset.sh $file20 10
+bash get_smaller_dataset.sh $file15 10
 bash mapreducer.sh $file10
