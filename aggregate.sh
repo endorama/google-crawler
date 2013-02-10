06e546b2ccd9d234a1b75bf79e6a15c244ec4375
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-02-10 19:46:43 +0100

    fix path for file to be processed

--- aggregate.sh
+++ aggregate.sh
@@ -3,9 +3,9 @@
 file="${1// /_}"
 
 # file50="*-50.mapreduced.csv"
-# file20="${file}-20.mapreduced.csv"
+# file20="*-20.mapreduced.csv"
 file15="*-15.mapreduced.csv"
-# file10="${file}-10.csv"
+# file10="*-10.mapreduced.csv"
 
 
 # bash lib/map_all_website/run.sh $file50
